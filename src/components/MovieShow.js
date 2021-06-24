5b3aa93b24148d1894b0def05ea959137211c034
demirbileku <udemirbilek2@gmail.com>
2021-06-24 10:14:05 +0300

    commit tree fb614e7f1c20c3903c9cd14778355a5290861d63

---
+++ src/components/MovieShow.js
@@ -0,0 +1,18 @@
+// ./src/components/MovieShow.js
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function MovieShow({ movies }) {
+    // call useParams to access the `params` from the url:
+    // the dynamic portion of our /movies/:movieId path
+    const params = useParams();
+    console.log(params);
+
+    return (
+        <div>
+            <h3>{movies[params.movieId].title}</h3>
+        </div>
+    );
+}
+
+export default MovieShow;
